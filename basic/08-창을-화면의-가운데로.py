8c21fca97201e4f0d877900e7ba85b0a76d424be
HAIL <s000715@naver.com>
2020-05-30 04:30:24 +0900

    200530

---
+++ basic/08-창을-화면의-가운데로.py
@@ -0,0 +1,34 @@
+## Ex 3-8. 창을 화면의 가운데로.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
+#창을 화면 가운데에 띄워보도록 하자.
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Centering')
+        self.resize(500, 350)
+        self.center()
+        #center() 메소드를 통해 창이 화면의 가운데에 위치하게 된다.
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        #frameGeometry() 메소드를 통해 창의 위치와 크기 정보를 가져오도록 하자
+        cp = QDesktopWidget().availableGeometry().center()
+        #사용하는 모니터 화면의 가운데 위치를 파악한다.
+        qr.moveCenter(cp)
+        #창의 직사각형 위치를 화면 중심 위치로 이동한다.
+        self.move(qr.topLeft())
+        #현재 창을 qr(화면의 중심으로 이동한 직사각형) 의 위치로 이동시킨다.
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
