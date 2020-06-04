0ab08068783796ed699686df6c62be501bef00e2
HAIL <s000715@naver.com>
2020-06-05 03:22:29 +0900

    200604

---
+++ widget/12-QPixmap.py
@@ -0,0 +1,43 @@
+## Ex 5-12. QPixmap.
+#Pixmap은 이미지 파일을 나타낼 때 사용하는 위젯이다.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        pixmap = QPixmap('../basic/test.png')
+        #담을 이미지를 선택하고 픽스맵 위젯을 생성한다.
+        pixmap=pixmap.scaledToHeight(300)
+        #픽스맵 위젯의 사이즈를 조절해준다. weight를 이용해 조절할 수도 있다.
+
+        lbl_img = QLabel()
+        lbl_img.setPixmap(pixmap)
+        #라벨을 만들고 setPixmap을 통해 라벨에 표시될 이미지로 위에서 생성한 픽스맵 위젯을 선택한다.
+        lbl_size = QLabel('Width: '+str(pixmap.width())+', Height: '+str(pixmap.height()))
+        lbl_size.setAlignment(Qt.AlignCenter)
+        #픽스맵 위젯의 너비와 높이를 표시할 라벨을 만들고 가운데 정렬한다.
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl_img)
+        vbox.addWidget(lbl_size)
+        self.setLayout(vbox)
+        #수평 박스 레이아웃을 통해 이미지 라벨과 사이즈 표시 라벨을 정렬한다.
+
+        self.setWindowTitle('QPixmap')
+        self.move(300, 300)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
---
+++ widget/13-QCalendarWidget.py
@@ -0,0 +1,48 @@
+## Ex 5-13. QCalendarWidget.
+#QCalendar위젯을 통해 사용자가 날짜를 선택할 수 있는 달력을 표시할 수 있다.
+#https://doc.qt.io/qt-5/qcalendarwidget.html
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QCalendarWidget
+from PyQt5.QtCore import QDate
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        cal = QCalendarWidget(self)
+        #QCalendarWidget 객체를 하나 생성한다.
+        cal.setGridVisible(False)
+        #setGridVisible(True/False) 로 날짜 사이의 그리드 표시 여부를 설정한다.
+        cal.clicked[QDate].connect(self.showDate)
+        #날짜를 클릭했을 때 showDate 메소드를 호출한다.
+
+        self.lbl = QLabel(self)
+        date = cal.selectedDate()
+        self.lbl.setText(date.toString())
+        #selectedDate는 선택된 날짜의 정보를 가지고 있으며, default는 현재 날짜이다.
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(cal)
+        vbox.addWidget(self.lbl)
+        #수직 박스 레이아웃을 이용해 달력과 날짜 표시 라벨을 정렬한다.
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('QCalendarWidget')
+        self.setGeometry(300, 300, 400, 300)
+        self.show()
+
+    def showDate(self, date): #위에서 호출한 showDate 메소드이다.
+        self.lbl.setText(date.toString())
+        #날짜를 클릭할 때마다 라벨의 텍스트가 선택된 날짜로 표시되도록 한다.
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
